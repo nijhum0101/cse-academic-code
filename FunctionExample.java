bdaeeb30b87698a60b5daddf199b1d6b2190416e
nijhum0101 <nnoshinanjumnijhum@gmail.com>
2026-01-21 11:27:02 +0600

    Create FunctionExample.java

---
+++ FunctionExample.java
@@ -0,0 +1,9 @@
+public class FunctionExample {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int sum = add(5, 7);
+        System.out.println("Sum: " + sum);
+    }
+}
